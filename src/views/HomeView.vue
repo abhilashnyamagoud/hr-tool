6405b60fdcd7664a104748625479bda67ee51fff
Abhilash Nyamagoud <abhilash.nyamagoud@wisseninfotech.com>
2022-04-25 09:27:27 +0530

    initial setup

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,15 +1,18 @@
 <template>
-  <hello-world />
+<NavigationBar/>
+  <!-- <hello-world /> -->
 </template>
 
 <script>
-  import HelloWorld from '../components/HelloWorld'
+  import HelloWorld from '../components/HelloWorld';
+  import NavigationBar from '../components/NavigationBar/NavigationBar.vue'
 
   export default {
     name: 'Home',
 
     components: {
       HelloWorld,
+      NavigationBar
     },
   }
 </script>
